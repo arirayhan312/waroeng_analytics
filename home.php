9373843838aabaed102aa3153a1ba35bac2b55e6
Rayhan Dwi Fajar <arirayhan312@gmail.com>
2021-12-02 07:26:42 +0700

    adding home section

--- home.php
+++ home.php
@@ -11,9 +11,22 @@
     <div class="jumbotron jumbotron-fluid">
       <div class="container">
         <h1 class="display-4">Spesialis dalam <span>Olah Data</span> dan <span>Konsultasi Riset</span></h1>
-        <h4 class="sub-title"><b>Waroeng Analytics</b> berdedikasi tinggi untuk membantu dan memastikan klien kami dalam mencapai tujuan mereka secara optimal dengan pengolahan dan pemanfaatan data yang lebih sempuran sehingga klien dapat memperoleh manfaat dan nilai penuh dari data.</h4>
+        <h4 class="sub-title"><b>Waroeng Analytics</b> berdedikasi tinggi untuk membantu dan memastikan klien kami dalam mencapai tujuan mereka secara optimal dengan pengolahan dan pemanfaatan data yang lebih sempurna sehingga klien dapat memperoleh manfaat dan nilai penuh dari data.</h4>
       </div>
     </div> 
+    </section>
+    <section id="article1">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+              <h1 id="article1title">Mengapa Waroeng Analytics ?</h1>
+              <img src="assets/images/image_article1.jpg" alt="ilustrasi dari proyeksi data" id="imgarticle1">
+          </div>
+          <div class="col-md-6">
+          <article><b>Waroeng Analytics</b> berdedikasi tinggi untuk membantu dan memastikan klien kami dalam mencapai tujuan mereka secara optimal dengan pengolahan dan pemanfaatan data yang lebih sempurna sehingga klien dapat memperoleh manfaat dan nilai penuh dari data.</article>
+          </div>
+        </div>
+      </div>
     </section>
     <!-- End of Jumbotron Section-->
 
